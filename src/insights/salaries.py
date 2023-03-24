e8ff335ac96721441780b65e361266028bf39983
Guilherme Garcia <garciaguig@gmail.com>
2023-03-24 16:00:20 -0300

    feat: Implementa a função 'get_min_salary'

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -20,21 +20,16 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
+    jobs = read(path)
+    salaries = set()
 
-    Must call `read`
+    for job in jobs:
+        if job["min_salary"].isnumeric():
+            salaries.add(int(job["min_salary"]))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    min_salary = min(salaries)
 
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    return min_salary
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:

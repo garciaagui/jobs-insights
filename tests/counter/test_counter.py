26e1a266ad782529eb5e821c2e2d0dd2070a0a92
Guilherme Garcia <garciaguig@gmail.com>
2023-03-24 19:43:07 -0300

    test: Altera o teste da função 'count_ocurrences'

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,46 +1,6 @@
 from src.pre_built.counter import count_ocurrences
-from unittest.mock import mock_open, patch
-import json
-import pytest
 
 
-@pytest.fixture
-def jobs_data():
-    return [
-        {
-            "job_title": "OT/ICS Systems Engineer",
-            "company": "Forescout Technologies Inc.",
-            "state": "NY",
-            "city": "New York",
-            "min_salary": "122296",
-            "max_salary": "148734",
-            "job_desc": "Description",
-            "industry": "Information Technology",
-            "rating": "3.4",
-            "date_posted": "2020-04-28",
-            "valid_until": "2020-06-07",
-            "job_type": "FULL_TIME",
-            "id": "10",
-        },
-        {
-            "job_title": "Principal Incident Response Consultant",
-            "company": "Crypsis Group",
-            "state": "NY",
-            "city": "New York",
-            "min_salary": "",
-            "max_salary": "",
-            "job_desc": "Description",
-            "industry": "Information Technology",
-            "rating": "4.8",
-            "date_posted": "2020-04-27",
-            "valid_until": "2020-06-07",
-            "job_type": "FULL_TIME",
-            "id": "12",
-        },
-    ]
-
-
-def test_counter(jobs_data):
-    with patch("builtins.open", mock_open(read_data=json.dumps(jobs_data))):
-        count = count_ocurrences("path", "2020")
-        assert count == 4
+def test_counter():
+    word_counter = count_ocurrences("data/jobs.csv", "Developer")
+    assert word_counter == 352

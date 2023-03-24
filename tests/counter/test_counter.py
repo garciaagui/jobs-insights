b54a68a4e015bbe62cd67486685d91d23ef71065
Guilherme Garcia <garciaguig@gmail.com>
2023-03-24 19:19:01 -0300

    test: Add teste para a função 'count_ocurrences'

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,46 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+import json
+import pytest
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def jobs_data():
+    return [
+        {
+            "job_title": "OT/ICS Systems Engineer",
+            "company": "Forescout Technologies Inc.",
+            "state": "NY",
+            "city": "New York",
+            "min_salary": "122296",
+            "max_salary": "148734",
+            "job_desc": "Description",
+            "industry": "Information Technology",
+            "rating": "3.4",
+            "date_posted": "2020-04-28",
+            "valid_until": "2020-06-07",
+            "job_type": "FULL_TIME",
+            "id": "10",
+        },
+        {
+            "job_title": "Principal Incident Response Consultant",
+            "company": "Crypsis Group",
+            "state": "NY",
+            "city": "New York",
+            "min_salary": "",
+            "max_salary": "",
+            "job_desc": "Description",
+            "industry": "Information Technology",
+            "rating": "4.8",
+            "date_posted": "2020-04-27",
+            "valid_until": "2020-06-07",
+            "job_type": "FULL_TIME",
+            "id": "12",
+        },
+    ]
+
+
+def test_counter(jobs_data):
+    with patch("builtins.open", mock_open(read_data=json.dumps(jobs_data))):
+        count = count_ocurrences("path", "2020")
+        assert count == 4

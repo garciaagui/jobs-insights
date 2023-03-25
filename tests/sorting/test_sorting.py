cd9f8f7f9595df029545eb52874aaf6f630472a1
Guilherme Garcia <garciaguig@gmail.com>
2023-03-25 15:51:40 -0300

    test: Add teste para a função 'sort_by'

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,35 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    input = [
+        {
+            "title": "Web developer",
+            "min_salary": 2000,
+            "max_salary": 5000,
+        },
+        {"title": "Front end developer", "min_salary": 1000, "max_salary": ""},
+        {"title": "Back end developer", "min_salary": "", "max_salary": 3000},
+        {
+            "title": "Full stack developer",
+            "min_salary": 4000,
+            "max_salary": 8000,
+        },
+    ]
+    sorted_list = [
+        {"title": "Front end developer", "min_salary": 1000, "max_salary": ""},
+        {
+            "title": "Web developer",
+            "min_salary": 2000,
+            "max_salary": 5000,
+        },
+        {
+            "title": "Full stack developer",
+            "min_salary": 4000,
+            "max_salary": 8000,
+        },
+        {"title": "Back end developer", "min_salary": "", "max_salary": 3000},
+    ]
+
+    sort_by(input, "min_salary")
+    assert input == sorted_list
